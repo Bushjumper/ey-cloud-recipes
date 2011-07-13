5b1e395edb0f152553ffe1857445b34c5003a2d9
Graeme Longman <graemelongman@gmail.com>
2011-07-13 17:34:15 +0100

    Uncommented resque recipe

--- cookbooks/main/recipes/default.rb
+++ cookbooks/main/recipes/default.rb
@@ -40,7 +40,7 @@
 #require_recipe "exim::auth"
 
 #uncomment to run the resque recipe
-#require_recipe "resque"
+require_recipe "resque"
 
 #uncomment to run the redis recipe
 #require_recipe "redis"
